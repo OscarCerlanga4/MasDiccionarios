e776abeadf4e31b76bcd1f68542bf08c147cc1b9
OscarCerlanga4 <oscarcerlanga@iesrioarba.es>
2024-11-06 11:12:10 +0100

    first commit

---
+++ 0.py
@@ -0,0 +1,19 @@
+#! C:\Users\alumno24\AppData\Local\Microsoft\WindowsApps\python.exe
+
+
+
+L = []
+
+
+D = {}
+
+
+if __name__ == "__main__":
+    
+    print(D.__dir__())
+    
+    print(D.__delattr__)
+    
+    print("\x1b[1;33m" + "color amarillo")
+    
+   
